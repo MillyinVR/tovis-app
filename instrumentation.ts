2b4c439b1d4aa1129bd06fd7dabf71ff835c33cc
Tori <millyinvr@gmail.com>
2026-04-30 17:36:49 -0700

    Revert "Merge pull request #21 from MillyinVR/copilot/implement-get-single-booking-handler"
    This reverts commit 10c912c4a048e86763c32bf266ea0cb448b7c8b2, reversing
    changes made to 190041e59450ad8809ecd5c6d4f6fd75679e0e21.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,8 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./sentry.server.config')
-    const { initLifecycleDriftSink } = await import('./lib/booking/driftSink')
-    initLifecycleDriftSink()
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
